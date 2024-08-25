9529d34c704e88bca9479887ec79fbaff8e52197
satyam <satyamb971@gmail.com>
2024-08-25 13:22:11 +0530

    add css file

--- App.js
+++ App.js
@@ -1,3 +1,3 @@
-const heading=React.createElement('h1', {} ,"this is heading tag")
+const heading=React.createElement('h1', {id:"heading"} ,"this is heading tag")
 const root=ReactDOM.createRoot(document.getElementById('root'))
 root.render(heading)
