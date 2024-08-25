96ebdc8a4aa8b34ebcc2455f388e2c2eec580574
satyam <satyamb971@gmail.com>
2024-08-25 13:31:13 +0530

    nested div

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
-const heading=React.createElement('h1', {id:"heading"} ,"this is heading tag")
+// const heading=React.createElement('h1', {id:"heading"} ,"this is heading tag")
+// const root=ReactDOM.createRoot(document.getElementById('root'))
+// root.render(heading)
+
+// nested
+
+
+const parent =React.createElement('div', {id:"parent"}, React.createElement('div',{id:"child"},React.createElement('h1', {id:"heading"} ,"this is heading tag")))
+console.log(parent)
 const root=ReactDOM.createRoot(document.getElementById('root'))
-root.render(heading)
+root.render(parent)
