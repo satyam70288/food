40122ba9637945d5afda99c4bfddecec7554a1ca
satyam <satyamb971@gmail.com>
2024-08-25 13:39:31 +0530

    multi child or sibling

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-// const heading=React.createElement('h1', {id:"heading"} ,"this is heading tag")
-// const root=ReactDOM.createRoot(document.getElementById('root'))
-// root.render(heading)
+const heading = React.createElement('h1', { id: "heading" }, "this is heading tag")
+const root1 = ReactDOM.createRoot(document.getElementById('root'))
+// root1.render(heading)
 
 // nested
 
-
-const parent =React.createElement('div', {id:"parent"}, React.createElement('div',{id:"child"},React.createElement('h1', {id:"heading"} ,"this is heading tag")))
+// ReactCreateElement is Object
+const parent = React.createElement('div', { id: "parent" }, React.createElement('div', { id: "child" }, React.createElement('h1', { id: "heading" }, "this is heading tag")))
 console.log(parent)
-const root=ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(parent)
+
+// multichild
+const parent = React.createElement('div', { id: "parent" }, React.createElement('div', { id: "child" },
+    [React.createElement('h1', { id: "heading" }, "this is heading tag"),
+    React.createElement('h2', { id: "heading" }, "this is heading2 tag"),
+    ]
+))
+console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(parent)
+
+// Object$$typeof:
+//  Symbol(react.element)key: nullprops: { id: 'parent', children: {… } }
+//   children: { $$typeof: Symbol(react.element), type: 'div', key: null, ref: null, props: {… }, … }
+//    id: "parent"[[Prototype]]: Objectref: nulltype: "div"_owner: null_store: { validated: false } _self: null_source: null[[Prototype]]:
+//     Object
